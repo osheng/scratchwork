8f91140f58279cf88b2e7af418d4b360b747840f
osheng <osheng@gmail.com>
2020-05-31 12:27:18 -0400

    is_sorted and contains pass tests

--- btree.py
+++ btree.py
@@ -2,40 +2,41 @@ class Btree(object):
     """
     A bare bones implementation of a binary tree
     """
+
     def __init__(self, value: int, lnode=None, rnode=None):
         self.value = value
         self.lnode = lnode
         self.rnode = rnode
-
-    def accept_value(self, min: int, max: int) -> bool:
-        """
-        Helper method for is_sorted
-        """
-        if self.val < min or self.value > max:
-            return False
-        if self.lnode is not None:
-            self.lnode.accept_value(min, self.value)
-        if self.rnode is not None:
-            self.rnode.accept_value(self.value, max)
 
     def is_sorted(self) -> bool:
         """
         Return True if the tree is sorted from left to right,
         smallest to largest, and False otherwise
         """
-        flag = True
-        # The first not makes sure you're returning a bool and not None
-        # The next two not's make sure you're retuning the correct bool
-        return not not not self.accept_value(min, max)
+        def accept(self, min_val=None, max_val=None) -> bool:
+            """
+            Helper function for is sorted
+            """
+            if self is None:
+                return True
+            if (min_val is not None and self.value < min_val) or \
+                    (max_val is not None and self.value > max_val):
+                # print("failed at {}. min was {}. max was {}".\
+                # format(self.value, min_val, max_val))
+                return False
+            return accept(self.lnode, min_val, self.value) and\
+                accept(self.rnode, self.value, max_val)
+
+        return accept(self)
 
     def contains(self, target: int) -> bool:
         """
         Return true if the tree contains the value and None otherwise
         Assume the tree is not sorted
         """
-        if self.value = target:
+        if self.value == target:
             return True
-        if self.lnode is not None:
-            self.lnode.contains(target)
-        if self.rnode is not None:
-            self.rnode.contains(target)
+        return (self.lnode is not None and self.lnode.contains(target)) or \
+            (self.rnode is not None and self.rnode.contains(target))
+
+    # TODO write an insert method and array_to_tree
--- btree_test.py
+++ btree_test.py
@@ -1,5 +1,26 @@
 import unittest
+from btree import Btree
+
 
 class Test_Btree(unittest.TestCase):
-        #TODO write some tests
-        #TODO also look up pdb for debugging from console
+    def setUp(self):
+        self.t1 = Btree(1)
+        self.t2 = Btree(2, lnode=self.t1)
+        self.t4 = Btree(4)
+        self.bst = Btree(3, lnode=self.t2, rnode=self.t4)
+        self.non_bst = Btree(3, lnode=self.t2, rnode=Btree(2))
+
+    def test_is_sorted(self):
+        self.assertFalse(self.non_bst.is_sorted())
+        self.assertTrue(self.t2.is_sorted())
+        self.assertTrue(self.bst.is_sorted())
+
+    def test_contains(self):
+        self.assertTrue(self.bst.contains(4))
+        self.assertFalse(self.bst.contains(-1))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# TODO also look up pdb for debugging from commandline
